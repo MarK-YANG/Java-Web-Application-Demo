18e52863aa49cd591e2a367838f9b4a969486ed0
Mark <emp.yangchunyu@gmail.com>
2017-04-02 13:37:39 +0800

    Add SLF4J(lo4j2)

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,7 +1,10 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Created by mark on 4/1/17.
@@ -10,8 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //@RestController combines @Controller and @ResponseBody, two annotations
 public class HelloController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/")
     public String index(){
+        logger.debug("This is a debug message");
+        logger.info("This is a info message");
+        logger.warn("This is a warn message");
+        logger.error("This is a error message");
         return "Greetings from Spring Boot!";
     }
 }
